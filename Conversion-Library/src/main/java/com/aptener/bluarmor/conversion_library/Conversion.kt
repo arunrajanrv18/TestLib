3040f0b84ec768859918f66382be0a0438288992
arunrajanrv <rajanarun017@gmail.com>
2024-08-21 16:22:47 +0530

    Initial commit

---
+++ Conversion-Library/src/main/java/com/aptener/bluarmor/conversion_library/Conversion.kt
@@ -0,0 +1,23 @@
+package com.aptener.bluarmor.conversion_library
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Float.pxToDp(): Dp {
+    val density = LocalDensity.current.density
+    return (this / density).dp
+}
+
+@Composable
+fun Dp.dpToPx(): Float {
+    val density = LocalDensity.current.density
+    return (this.value * density)
+}
+
+
+
+
+
